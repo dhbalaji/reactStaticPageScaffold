6277912b39d67781667568777825b545bebe595a
D Balaji <pessebalu@gmail.com>
2020-04-03 22:00:00 +0530

    Dark mode picked up from OS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React from 'react'
 import './App.scss'
 import LiveClock from './components/LiveClock'
+import { shouldUseDarkMode } from './common/helper'
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img
-                    src={process.env.PUBLIC_URL + '/img/logo.svg'}
-                    className="App-logo"
-                    alt="logo"
-                />
-                <p className="App-env"> > {process.env.REACT_APP_CODE}</p>
-            </header>
-            <main className="clock-wrapper centre-align">
-                <LiveClock />
-            </main>
-        </div>
-    )
+// TODO: Put pref to local storage or check local storage option
+class App extends React.Component {
+    state = {
+        isDarkModeEnabled: shouldUseDarkMode()
+    }
+
+    render() {
+        const { isDarkModeEnabled } = this.state
+        let appClassName = isDarkModeEnabled ? 'darkTheme' : 'lightTheme'
+        appClassName = `${appClassName} App`
+        return (
+            <div className={appClassName}>
+                <header className="App-header">
+                    <img
+                        src={process.env.PUBLIC_URL + '/img/logo.svg'}
+                        className="App-logo"
+                        alt="logo"
+                    />
+                    <p className="App-env"> > {process.env.REACT_APP_CODE}</p>
+                </header>
+                <main className="clock-wrapper centre-align">
+                    <LiveClock />
+                </main>
+            </div>
+        )
+    }
 }
 
 export default App
