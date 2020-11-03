98679fffc01bce9dec59f101cd4365aaae520519
D Balaji <pessebalu@gmail.com>
2020-11-03 09:39:28 +0530

    Added sentry

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import './index.scss'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import 'typeface-roboto'
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://c797c4f00b82493e9e9f20e6d393620d@o470945.ingest.sentry.io/5502298",
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
     <React.StrictMode>
